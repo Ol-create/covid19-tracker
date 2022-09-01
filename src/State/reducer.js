a826097147fc318e2d7646dc4482786788623415
Ol-create <oluola96@gmail.com>
2022-09-01 12:20:20 -0600

    Feat: create action creator function

--- src/State/reducer.js
+++ src/State/reducer.js
@@ -3,3 +3,19 @@ import React from 'react';
 // Creact Action Type
 const FETCH_API = 'FETCH_API';
 const GET_DETAILS = 'GET_DETAILS';
+
+// Create action creator function;
+const fetchApi = () => {
+	return {
+		type: FETCH_API,
+		payload: [],
+	}
+}
+
+const getDetails = () => {
+	return {
+		type: GET_DETAILS,
+		payload: [],
+	}
+}
+
